7e6f26136032a996d3a243c0345d6d0c91a74c48
Remon Emad <remonemad658@gmail.com>
2022-12-07 23:38:11 +0200

    create recyclerview on fragment but not working (no MVVM)

---
+++ app/src/main/java/com/example/wagba/Restaurant.java
@@ -0,0 +1,12 @@
+package com.example.wagba;
+
+public class Restaurant {
+
+    String RName;
+    int RImage;
+
+    public Restaurant(String RName, int RImage) {
+        this.RName = RName;
+        this.RImage = RImage;
+    }
+}
--- app/src/main/java/com/example/wagba/view/MainActivity.java
+++ app/src/main/java/com/example/wagba/view/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.wagba.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.wagba.R;
+import com.example.wagba.RestaurantFragment;
 import com.example.wagba.databinding.ActivityMainBinding;
 import com.example.wagba.view.Login;
 import com.example.wagba.viewmodel.LogOutViewModel;
@@ -26,13 +34,17 @@ public class MainActivity extends AppCompatActivity {
     Intent loginInt;
 //    DatabaseReference myRef;
 
+    TextView b1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        View view = binding.getRoot();
-        setContentView(view);
+//        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        View view = binding.getRoot();
+        setContentView(R.layout.activity_main);
 
+        replaceFragment(new RestaurantFragment());
+/*
         logOutViewModel= new ViewModelProvider(this).get(LogOutViewModel.class);
         logOutViewModel.getLoggedOutMutualLiveData().observe(this, new Observer<Boolean>() {
             @Override
@@ -40,28 +52,45 @@ public class MainActivity extends AppCompatActivity {
                 startLoginActivity();
                 return;
             }
-        });
+        });*/
 
         auth= FirebaseAuth.getInstance();
         currentUser = auth.getCurrentUser();
-        if(currentUser == null){
+        /*if(currentUser == null){
             startLoginActivity();
             return;
-        }
+        }*/
 
 //        database = FirebaseDatabase.getInstance();
 //        myRef = database.getReference("testx");
 //
 //        myRef.setValue("database connected");
 
-        binding.logoutButton.setOnClickListener(new View.OnClickListener() {
+//        binding.logoutButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                logout();
+//            }
+//        });
+
+        b1= findViewById(R.id.logoutButton);
+        b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                logout();
+                Toast.makeText(MainActivity.this, "test test", Toast.LENGTH_SHORT).show();
             }
         });
 
 
+
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        Log.d("testfrag","here");
+        FragmentManager fragmentManager= getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction= fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
     }
 
     private void startLoginActivity(){
